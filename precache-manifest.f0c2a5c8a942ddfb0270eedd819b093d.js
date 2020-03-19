6da73b61c08c7d2a9e56dbbebaa6c75ba884854c
Yameen <you@example.com>
2020-03-19 17:58:17 +0600

    Updates

---
+++ precache-manifest.f0c2a5c8a942ddfb0270eedd819b093d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15767a06e170ab60457be84c2cdc7bc2",
+    "url": "/monsters-Rolodex/index.html"
+  },
+  {
+    "revision": "911a99bf4816bd4d92ae",
+    "url": "/monsters-Rolodex/static/css/main.35bfb85e.chunk.css"
+  },
+  {
+    "revision": "30436d15d072549122d4",
+    "url": "/monsters-Rolodex/static/js/2.0d232ef2.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/monsters-Rolodex/static/js/2.0d232ef2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "911a99bf4816bd4d92ae",
+    "url": "/monsters-Rolodex/static/js/main.13b1f505.chunk.js"
+  },
+  {
+    "revision": "1d6e989f9fc0fda671c6",
+    "url": "/monsters-Rolodex/static/js/runtime-main.1856b93d.js"
+  }
+]);
